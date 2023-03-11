da22fbf5d82e5d77de33761b0dfbf6c8f8849688
khalid <khalid@aveni.ai>
2023-03-11 12:23:33 +0000

    Changes how we deal with constants
    now we use the config file to write the constants instead of writing directly to constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,14 @@
-max_seq_len = 256
-min_seq_len = 4
-min_freq = 100
-context_size = 4
+import yaml
+with open("../config.yaml", 'r') as stream:
+    config = yaml.safe_load(stream)
+
+
+max_seq_len = config["max_seq_len"]
+min_seq_len = config["min_seq_len"]
+min_freq = config["min_freq"]
+context_size = config["context_size"]
+
+model_name = config["model_name"]
+batch_size = config["batch_size"]
+epochs = config["epochs"]
+learning_rate = config["learning_rate"]
